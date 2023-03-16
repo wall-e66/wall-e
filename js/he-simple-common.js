ef0c7d3f61785c372ac9e3b86dce5a799bb32f1a
wall-e66 <82195083+wall-e66@users.noreply.github.com>
2023-03-16 20:35:57 +0800

    Add files via upload

---
+++ js/he-simple-common.js
@@ -0,0 +1,10 @@
+(function (d) {
+  var c = d.createElement('link')
+  c.rel = 'stylesheet'
+  c.href = 'http://www.wall-e.cn/web/js/he-simple.css?v=1.4.0'
+  var s = d.createElement('script')
+  s.src = 'http://www.wall-e.cn/web/js/he-simple.js?v=1.4.0'
+  var sn = d.getElementsByTagName('script')[0]
+  sn.parentNode.insertBefore(c, sn)
+  sn.parentNode.insertBefore(s, sn)
+})(document)
